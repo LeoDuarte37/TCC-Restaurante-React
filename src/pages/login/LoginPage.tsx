0d6dfa842f10f160086bc51b80e59804895d28b7
Leo Duarte <leonardo.r.s.duarte@gmail.com>
2024-05-20 12:42:34 -0300

    Finalizando tela de login

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,18 +1,12 @@
 import './LoginPage.css'
-import ModalLogin from '../../components/login/modal/ModalLogin';
-import { ChefHat } from '@phosphor-icons/react';
+import FormLogin from '../../components/login/forms/FormLogin';
 
 function LoginPage() {
     return (
         <div className="caixa">
             <div className="caixaContent">
-                <div className='caixaTitle'>
-                    <h1>Bon Patron</h1>
-                    <ChefHat size={45} color='white'/>
-                </div>
-                <h2>Logar como:</h2>
                 <div>
-                    <ModalLogin mostrar={false}/>
+                    <FormLogin />
                 </div>
             </div>
         </div>
